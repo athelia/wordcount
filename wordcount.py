06576a045c9b1c8d8c8c30cddfce7ec267dc9931
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-07 12:13:39 -0700

    Created count_words() function

--- wordcount.py
+++ wordcount.py
@@ -8,3 +8,16 @@ for line in file
 
 """
 
+def count_words(path):
+
+    file = open(path)
+    word_count = {}
+
+    for line in file:
+        word_list = line.split()
+        for word in word_list:
+            word_count[word] = word_count.get(word, 0) + 1
+
+    file.close()
+    return word_count
+    
