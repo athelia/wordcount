1b0c0d60ede5b210f3ddb2e5cbc0802b68af37f8
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-07 12:24:01 -0700

    Refactor

--- wordcount.py
+++ wordcount.py
@@ -15,7 +15,7 @@ def count_words(path):
 
     for line in file:
         word_list = line.split()
-        for word in word_list:
+        for word in word_list: #[word.lower for word in word_list]
             word = word.lower()
             word_count[word] = word_count.get(word, 0) + 1
 
